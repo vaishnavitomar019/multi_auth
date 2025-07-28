a064b185bc39c60f0315693fba7d50f646a14619
vaishanvi Tomar <vaishanvi-tomar@RT0217245>
2025-07-28 16:30:29 +0530

    ensure that sidebar and navbar only disply when the user is logged in

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,14 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { AuthService } from './core/services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent,SidebarComponent],
+  imports: [RouterOutlet, NavbarComponent, SidebarComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'client';
+
+  constructor(public authService: AuthService) { }
+
 }
