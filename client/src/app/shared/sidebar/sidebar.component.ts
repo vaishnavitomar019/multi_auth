e95d140bcb5b4e25b73b1ffc64847205f8e8496a
vaishanvi Tomar <vaishanvi-tomar@RT0217245>
2025-08-05 16:31:51 +0530

    ui chnages done-sticky navbar,sidebar ui enhancemet  scroll only content area

--- client/src/app/shared/sidebar/sidebar.component.ts
+++ client/src/app/shared/sidebar/sidebar.component.ts
@@ -14,7 +14,16 @@ export class SidebarComponent {
   isCollapsed = false;
 
   constructor(private authService:AuthService, private router: Router) {};
-  
+  menuItems = [
+  { label: 'Dashboard', icon: 'bi bi-grid-fill', route: '/dashboard' },
+  { label: 'AI Summarizer', icon: 'bi bi-robot', route: '/summarizer' },
+  { label: 'Text Translation', icon: 'bi bi-translate', route: '/translation' },
+  { label: 'Grammar Correction', icon: 'bi bi-pencil-square', route: '/correction' },
+  { label: 'Code Explanation', icon: 'bi bi-code-slash', route: '/code-explanation' },
+  { label: 'Resume/JD Matcher', icon: 'bi bi-file-earmark-person', route: '/resume-matcher' },
+  { label: 'User', icon: 'bi bi-person-circle', route: '/user' },
+];
+
 
   toggleSidebar(): void {
     this.isCollapsed = !this.isCollapsed;
