c7237999d9e509c56a0cde0a15f4cc5fde14ac08
vaishanvi Tomar <vaishanvi-tomar@RT0217245>
2025-08-06 16:55:00 +0530

    backend url update2

--- client/src/app/environments/environments.ts
+++ client/src/app/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // apiUrl: 'http://localhost:5000/api' 
-  apiUrl: 'https://backend-l42h.onrender.com/'
+  apiUrl: 'https://backend-l42h.onrender.com'
 };
